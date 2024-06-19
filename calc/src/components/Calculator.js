f4d582915b0991907ecd02ab4880967a1bca058c
Mr Tech <koechfestus003gmail@.com>
2024-06-19 22:39:45 +0300

    first commit

---
+++ calc/src/components/Calculator.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import './Calculator.css';
+import { ClearAll } from "@mui/icons-material";
+const Calculator = () => {
+    const [Display, setDisplay]=useState('0');
+    const [currentValue,setCurrentValue]=useState('');
+    const [previousValue,setPreviousValue]=useState('');
+    const [operator,setOperator]=useState('');
+
+    const handleButtonClick =(value) => {
+        if(value === '='){
+            calculateResult();
+        }
+        else if (value === 'C') {
+            ClearAll;
+            
+        }
+    };
+    return ( 
+
+     );
+}
+ 
+export default Calculator;
